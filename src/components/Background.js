6dc8379276154da97d6895f7ed25ab71cd9e9a54
Luis <luisrc91@live.com>
2023-03-31 21:47:21 -0400

    added style

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/esm/Row";
 
-export default function Background() {
+function Background() {
   return (
-    <div>Background</div>
-  )
+    <div style={{display: 'flex', flexDirection: 'row', flexBasis:'auto'}}>
+      <div>
+      <Card style={{ width: "1300px", minHeight: "250px", marginBottom: "50px", backgroundColor: "#DEEED6", border: 'none'}} className="text-black">
+        
+        <Card.ImgOverlay style={{ top: "auto", width: "578px", marginLeft: "100px", marginBottom: "50px", marginTop: "75px"}}>
+          <Card.Title style={{ fontSize: "2.5em", fontWeight: "bold" }}>Order groceries for delivery or pickup today</Card.Title>
+          <Card.Text style={{ fontSize: "1em" }}>
+            Whatever you want from local stores, brought right to your door.
+          </Card.Text>
+          {/* <Card.Text>Last updated 3 mins ago</Card.Text> */}
+        </Card.ImgOverlay>
+        
+      </Card>
+      </div>
+      <div>
+        <Card.Img  style={{ height: "250px", width: "20%",position: "absolute", backgroundSize: "contain", backgroundRepeat: "no-repeat" }}
+          src="https://www.instacart.com/image-server/x428/www.instacart.com/assets/homepage/homepage_background_full_m3_cropped-8d2d286263821da7decd7c61fb1db1eb0e3dec13e0c356277d6d3cb7484c024a.jpg"
+          alt="Card image"
+        />
+
+      </div>
+    </div>
+  );
 }
+
+export default Background;
--- src/components/StoreListing.js
+++ src/components/StoreListing.js
@@ -6,8 +6,19 @@ import Container from "react-bootstrap/Container";
 
 export default function StoreListing() {
   return (
-    <div>
-      <h2>Browse stores in Houston</h2>
+    <div style={{ margin: "auto" }}>
+      <h2
+        style={{
+          fontSize: "36px",
+          lineHeight: "50px",
+          marginTop: "20px",
+          textAlign: "center",
+          fontWeight: "normal",
+        }}
+      >
+        Browse stores in Houston
+      </h2>
+
       <Container style={{ marginTop: "40px", textAlign: "center" }}>
         <Row
           style={{
@@ -16,37 +27,100 @@ export default function StoreListing() {
             justifyContent: "space-evenly",
           }}
         >
-          <Row>
-            <Col xs=".5">
-              <Image src="[IMG SRC]" roundedCircle />
+          <row style={{ width: "400px", boxShadow: "0px 2px 8px #9E9E9E",'borderRadius':'10px',  }}>
+            {" "}
+            <Col sm="1">
+              <Row
+                style={{
+                  // border: "solid black 1px",
+                  height: "100px",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  
+                }}
+              >
+                <Col xs=".5">
+                  <Image
+                    style={{
+                      height: "55px",
+                      width: "50px",
+                      border: "1px solid #E8E9EB",
+                    }}
+                    src="https://www.instacart.com/image-server/58x58/filters:fill(FFF,true):format(webp)/www.instacart.com/assets/domains/warehouse/logo/12/2774c343-1319-4720-a54f-0c46e93f956c.png"
+                    roundedCircle
+                  />
+                </Col>
+                <Col>
+                  <h5 style={{ fontWeight: "bold", padding:'0px 10px;'  }}>ALDI</h5>
+                  <p style={{ margin:'0', padding:'0px 10px;'  }}>Delivery * Pickup</p>
+                </Col>
+              </Row>
             </Col>
-            <Col>
-              <h5 style={{ fontWeight: "bold" }}>ALDI</h5>
-              <p>Delivery * Pickup</p>
+          </row>
+
+          <row style={{ width: "400px", boxShadow: "0px 2px 8px #9E9E9E",borderRadius:'10px' }}>
+            {" "}
+            <Col sm="1">
+              <Row
+                style={{
+                  // border: "solid black 1px",
+                  height: "100px",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                }}
+              >
+                <Col xs=".5">
+                  <Image
+                    style={{
+                      height: "55px",
+                      width: "50px",
+                      border: "1px solid #E8E9EB",
+                    }}
+                    src="https://www.instacart.com/image-server/58x58/filters:fill(FFF,true):format(webp)/www.instacart.com/assets/domains/warehouse/logo/279/59eb00b1-2e19-4912-bf2e-0c7a1f71f8b1.png"
+                    roundedCircle
+                  />
+                </Col>
+                <Col style={{}}>
+                  <h5 style={{fontSize:'20px', fontWeight: "bold", margin:'0',padding:'0px 10px;'  }}>Sprouts Farmers Market</h5>
+                  <p style={{ margin:'0',padding:'0px 10px;'  }}> Delivery * Pickup</p>
+                </Col>
+              </Row>
             </Col>
-          </Row>
-          <Col sm="1"></Col>
-          <Row>
-            <Col xs=".5">
-              <Image src="[IMG SRC]" roundedCircle />
+          </row>
+
+          <row style={{ borderRadius:'10px', width: "400px", boxShadow: "0px 2px 8px #9E9E9E" }}>
+            {/* rgba(0,0,0,0.16) 0px 2px 8px; */}
+            <Col sm="1">
+              <Row
+                style={{
+                  // border: "solid black 1px",
+                  height: "100px",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                }}
+              >
+                <Col xs=".5">
+                  <Image
+                    style={{
+                      height: "55px",
+                      width: "50px",
+                      border: "1px solid #E8E9EB",
+                    }}
+                    src="https://www.instacart.com/image-server/58x58/filters:fill(FFF,true):format(webp)/www.instacart.com/assets/domains/warehouse/logo/57/29520839-7042-45a0-af82-54f973b4abe5.png"
+                    roundedCircle
+                  />
+                </Col>
+                <Col md="6" style={{display:'column'}}>
+                  <h5 style={{  fontWeight: "bold", margin:'0',padding:'0px 10px;'  }}>Publix</h5>
+                  <p style={{  margin:'0', padding:'0px 10px;' }}>Delivery</p>
+                </Col>
+              </Row>
             </Col>
-            <Col>
-              <h5 style={{ fontWeight: "bold" }}>Sprouts Farmers Market</h5>
-              <p>Delivery * Pickup</p>
-            </Col>
-          </Row>
-          <Col sm="1"></Col>
-          <Row>
-            <Col xs=".5">
-              <Image src="[IMG SRC]" roundedCircle />
-            </Col>
-            <Col md="6">
-              <h5 style={{ fontWeight: "bold" }}>Kroger</h5>
-              <p>Delivery</p>
-            </Col>
-          </Row>
+          </row>
         </Row>
       </Container>
     </div>
   );
 }
+
+// style={{border:'solid black 1px', width:'500px' }}
